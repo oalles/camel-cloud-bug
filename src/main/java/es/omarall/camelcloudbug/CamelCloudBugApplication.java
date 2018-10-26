e958049c5124d942e47e5536e951c0ef66083a4d
Omar Alles <oalles@gmail.com>
2018-10-26 19:38:49 +0200

    Removes application runner

--- src/main/java/es/omarall/camelcloudbug/CamelCloudBugApplication.java
+++ src/main/java/es/omarall/camelcloudbug/CamelCloudBugApplication.java
@@ -1,11 +1,9 @@
 package es.omarall.camelcloudbug;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -14,12 +12,5 @@ public class CamelCloudBugApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CamelCloudBugApplication.class, args);
-    }
-
-    @Bean
-    ApplicationRunner runner(StoreClient client, StoreUpdater updater) {
-        return (args) -> {
-            log.warn("Done!");
-        };
     }
 }
