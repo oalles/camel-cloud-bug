35f7ee104c73337a259f15006d4b4a0258fc3dd5
Omar Alles <oalles@gmail.com>
2018-10-26 19:34:27 +0200

    Updates to last version and remove eureka client dependency

--- src/main/java/es/omarall/camelcloudbug/CamelCloudBugApplication.java
+++ src/main/java/es/omarall/camelcloudbug/CamelCloudBugApplication.java
@@ -1,20 +1,13 @@
 package es.omarall.camelcloudbug;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.camel.Ordered;
-import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.spring.SpringRouteBuilder;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-import org.springframework.core.annotation.Order;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableFeignClients
 @Slf4j
 public class CamelCloudBugApplication {
--- src/main/java/es/omarall/camelcloudbug/StoreUpdater.java
+++ src/main/java/es/omarall/camelcloudbug/StoreUpdater.java
@@ -1,12 +1,10 @@
 package es.omarall.camelcloudbug;
 
 import org.bouncycastle.util.Store;
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.util.List;
 
 @FeignClient("stores-updater")
 public interface StoreUpdater {
